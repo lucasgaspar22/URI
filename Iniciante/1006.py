9072876a67d9b7b360c183d82b1e6a521b1bf7fb
edepluc <lucas.gaspar@ericsson.com>
2019-07-12 15:50:02 -0300

    Problems 1001 to 1008

---
+++ Iniciante/1006.py
@@ -0,0 +1,6 @@
+a = float(input())
+b = float(input())
+c = float(input())
+mean = ( (a*2)+(b*3) +(c*5) )/10
+
+print("MEDIA = %.1f"%mean)
---
+++ Iniciante/1008.py
@@ -0,0 +1,8 @@
+number = int(input())
+hours = int(input())
+hourValue = float(input())
+
+salary = hours*hourValue
+
+print("NUMBER =",number)
+print("SALARY = U$ %.2f" %salary)
